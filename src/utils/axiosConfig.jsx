c306e9b0f677ca3b29e48dff309dce8255056ae8
Afzunov <115172984+MustafoAfzunov@users.noreply.github.com>
2024-12-30 13:29:13 +0600

    Update axiosConfig.jsx

--- src/utils/axiosConfig.jsx
+++ src/utils/axiosConfig.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 // Create a single Axios instance
 const axiosInstance = axios.create({
   // baseURL: 'http://localhost:8080',
-  baseURL: 'financemanager-production-27ab.up.railway.app',
+  baseURL: 'https://financemanager-production-27ab.up.railway.app',
 });
 
 // Request interceptor to add the Authorization header
